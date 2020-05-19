1935c8e1385fdd183f787bff3d12b27da487e9d1
Joseph D. Romano <jdromano2@gmail.com>
2020-05-19 14:39:36 -0400

    First commit for refactor

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Hibachi',
+    version='2.0-alpha',
+    description='Data simulation software that creates data sets with particular characteristics',
+    author='Pete Schmidt and Joseph D. Romano',
+    author_email='joseph.romano@pennmedicine.upenn.edu',
+    url='https://github.com/EpistasisLab/hibachi'
+)
